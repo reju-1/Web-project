d0945ce9558398ba1bf9761fd34a1e208c5ec358
reju-1 <rahmed1445@gmail.com>
2023-12-17 01:43:46 +0600

    Prifile is done

--- pages/profile.php
+++ pages/profile.php
@@ -4,6 +4,9 @@ include './php_utility/connection.php';
 session_start();
 $emailID = $_SESSION['email'];
 
+$name = '';
+$img = '';
+
 // Prepare and execute query to get names and Picture of friends based on user email
 $stmt = $conn->prepare("SELECT CONCAT(u.firstName, ' ', u.lastName) AS name, u.picture, p.content, p.likeCount, p.id
                              FROM users u
@@ -15,7 +18,7 @@ $stmt->execute();
 
 $post = $stmt->get_result();
 $profileFeed = $post->fetch_assoc();
-print_r($profileFeed);
+// print_r($profileFeed);
 
 $stmt->close();
 $conn->close();
@@ -29,106 +32,95 @@ $conn->close();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="./css/profile.css">
     <title>Document</title>
 </head>
 
 <body>
-    <div class="main_div">
 
+    <div class="root">
 
-        <!-- LEFT DIV -->
-
-
-        <div class="left_div">
-            <div class="Profile">
-                <button class="btn"><i class="fa fa-profile"></i> Profile</button>
-
-                <p> Fabian's lifestyle is quite active. He works out three times a week. His fitness routine is a
-                    healthy mix of strength-training and cardio. Apart from fitness, his priority is a healthy and
-                    balanced diet. He have some sort of a meal plan, but he is quite fliexible with it. He prefers to do
-                    shopping online because it is convenient and less time-consuming, as he can do it whenever it suits
-                    him.</p>
-            </div>
-
-            <div class="post">
-                <button class="btn"><i class="fa fa-post"></i> Post</button>
-                <p> 21 August 2022<br><br>
-                    Fabian's lifestyle is quite active. He works out three times a week. His fitness routine is a
-                    healthy mix of strength-training and cardio. Apart from fitness, his priority is a healthy and
-                    balanced diet. He has some sort of a meal plan, but he is quite flexible with it.
-
-                </p>
-
-                <button class="btn"><i class="fa fa-like"></i> Like</button> 209 like
-                <button class="btn"><i class="fa fa-comment"></i> Comment</button> 7 comment
-                <button class="btn"><i class="fa fa-share"></i> Share</button> Boost
-                <br>
-                <br>
-                <br>
-                <br>
-                <P>
-                    13th Auguest,2022<br><br>
-                    I left my heart in the send
-                    <br>(add image hare)
-                </p>
-                <img src="./../assets/profiles/pic-1.jpg" alt="Trulli" width="30" height="70">
-
-                <button class="btn"><i class="fa fa-like"></i> Like</button> 29 like
-                <button class="btn"><i class="fa fa-comment"></i> Comment</button>5 comment
-                <button class="btn"><i class="fa fa-share"></i> Share</button> Boost
-                <P>
-                </P>
-            </div>
-            <!--in needed then add labal code is (<label class="label20">Submit Mid Here</label>)-->
-
-
-
-
-
-
+        <div class="sidebar">
+            <h1 id="logo">Orange</h1>
+            <ul>
+                <li><a href="./home.php">Home</a></li>
+                <li style="background-color: rgb(107, 138, 170);"><a href="./profile.php">Profile</a></li>
+                <li><a href="./messenger.php">Messages</a></li>
+                <li><a href="./event/calendar.php">Event</a></li>
+                <li><a href="./php_utility/logout.php">Log Out</a></li>
+            </ul>
         </div>
 
+        <div class="feed-boi-root">
 
-        <!-- RIGHT DIV -->
+            <div class="feed">
+
+                <div class="post-container">
+                    <form id="postForm">
+                        <textarea id="postData" placeholder="Enter your post here..."></textarea>
+                        <button type="submit">Post</button>
+                    </form>
+                </div>
+
+                <?php if ($post->num_rows > 0) {
+                    while ($row = $post->fetch_assoc()) { ?>
+                        <div class="post">
+                            <img src="../assets/profiles/<?php echo $row['picture']; ?>" alt="">
+
+                            <div class="post-content">
+                                <?php
+                                $name = $row['name'];
+                                $img = $row['picture'];
+                                ?>
+                                <h3>
+                                    <?php echo $name ?>
+                                </h3>
+                                <p>
+                                    <?php echo $row['content']; ?>
+                                </p>
 
 
-        <div class="right_div">
-            <div class="upper">
 
-                <img src="./../assets/profiles/pic-1.jpg" alt="Avatar" style="width:200px">
-                <br>(add same image here)
-
-
+                                <div class="interactions">
+                                    <button class="like-btn" id="<?php echo $row['id']; ?>">Like</button>
+                                    <span class="like-count">
+                                        <?php echo $row['likeCount'] . ' Likes '; ?>
+                                    </span>
+                                    <button class="comment-btn">Comment</button>
+                                    <span class="comment-count">0 Comments</span>
+                                </div>
+                            </div>
+                        </div>
+                    <?php }
+                } else {
+                    echo "no post found";
+                } ?>
             </div>
-
-            <div class="btns">
-
-                <p>
-
-
-                    Name <br>
-                    <b>Sheik Rakin</b><br><br>
-                    Age <br>
-                    <b>23 years old</b><br><br>
-                    Occupation <br>
-                    <b>Web developer</b><br><br>
-                    Hobbies <br>
-                    <b>Rock climbing, bboard games.</b><br><br>
-                    Category <br>
-                    <b>Studen</b><br><br>
-                    Batch <br>
-                    <b>202</b><br><br>
-
-
-
-                </p>
+            <div class="bio-container">
+                <img src="../assets/profiles/<?php echo $img ?>" alt="">
+                <div class="bio">
+                    <p>
+                        Name <br>
+                        <b>
+                            <?php echo $name ?>
+                        </b><br><br>
+                        Age <br>
+                        <b>23 years old</b><br><br>
+                        Occupation <br>
+                        <b>Web developer</b><br><br>
+                        Hobbies <br>
+                        <b>Rock climbing, bboard games.</b><br><br>
+                        Category <br>
+                        <b>Studen</b><br><br>
+                        Batch <br>
+                        <b>202</b><br><br>
+                    </p>
+                </div>
             </div>
-
-
         </div>
+
     </div>
 </body>
+<script src="./js/home.js"></script>
 
 </html>
